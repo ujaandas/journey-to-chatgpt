c96867c7eb7bb9a4cade45efaee2c6122925cf5c
Ujaan Das <ujaandas03@gmail.com>
2026-02-26 10:43:28 +0000

    add graphing stuff

---
+++ linear-regression/graph.h
@@ -0,0 +1,13 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+typedef struct Point {
+  double x;
+  double y;
+} Point;
+
+Point* makePoints(double x[], int xLen, double y[], int yLen);
+
+void drawGraph(Point points[], int pointLen);
+
+#endif // GRAPH_H
